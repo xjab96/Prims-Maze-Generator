5f205c1a872e9d8ebd758201e39e0a82c5ed3e53
xjab96 <patrickcooper96@outlook.com>
2020-03-25 12:15:39 +0000

    Revamped some names. Removed old code

--- MazeGeneratorSections.cs
+++ MazeGeneratorSections.cs
@@ -9,19 +9,13 @@ public class MazeGeneratorSections : MonoBehaviour
 
     public Vector2Int mazeDimensions = new Vector2Int(9, 9);
     public Vector2Int mazeSubGridDimensions = new Vector2Int(3, 3);
-    private Vector2Int mazeGridDimensions;
+    private Vector2Int mazeFullDimensions;
 
-    private List<MazePiece>[,] mazeGrid;
-    private List<MazePiece>[,] openGrids;
-    private List<MazePiece>[,] closedGrids;
+    private List<List<MazePiece>> unvisitedGrids = new List<List<MazePiece>>();
+    private List<List<MazePiece>> openGrids;
+    private List<List<MazePiece>> closedGrids;
 
-    private List<List<MazePiece>> gridPieces = new List<List<MazePiece>>();
-    private List<List<MazePiece>> openGridPieces;
-    private List<List<MazePiece>> closedGridPieces;
-
-    private List<MazePiece> subgridPieces = new List<MazePiece>();
-    private List<MazePiece> openList = new List<MazePiece>();
-    private List<MazePiece> closedList = new List<MazePiece>();
+    private List<MazePiece> mazePieces = new List<MazePiece>();
 
     private class MazePiece
     {
@@ -34,10 +28,7 @@ public class MazeGeneratorSections : MonoBehaviour
     }
     void Start()
     {
-        mazeGridDimensions = new Vector2Int(mazeDimensions.x * mazeSubGridDimensions.x, mazeDimensions.y * mazeSubGridDimensions.y);
-        mazeGrid = new List<MazePiece>[mazeDimensions.x, mazeDimensions.y];
-        openGrids = mazeGrid;
-        closedGrids = mazeGrid;
+        mazeFullDimensions = new Vector2Int(mazeDimensions.x * mazeSubGridDimensions.x, mazeDimensions.y * mazeSubGridDimensions.y);
 
         GenerateEmptyMaze();
         InstantiateMaze();
@@ -48,27 +39,21 @@ public class MazeGeneratorSections : MonoBehaviour
         {
             for (int x = 0; x < mazeDimensions.x; x++)
             {
-                //mazeGrid[x, z] = new List<MazePiece>();
-                gridPieces.Add(new List<MazePiece>());
-
+                unvisitedGrids.Add(new List<MazePiece>());
             }
         }
-        Debug.Log(gridPieces.Count);
 
         //Generate initial maze grid
-        for (int z = 1; z <= mazeGridDimensions.y; z++)
+        for (int z = 1; z <= mazeFullDimensions.y; z++)
         {
-            for (int x = 1; x <= mazeGridDimensions.x; x++)
+            for (int x = 1; x <= mazeFullDimensions.x; x++)
             {
-                subgridPieces.Add(new MazePiece(new Vector2(x, z)));
-
-                int testX = (int)Mathf.Clamp((x - 1) / mazeSubGridDimensions.x, 0, Mathf.Infinity);
-                int testZ = (int)Mathf.Clamp((z - 1) / mazeSubGridDimensions.y, 0, Mathf.Infinity);
-
-
-                gridPieces[testX + (testZ * mazeDimensions.x)].Add(subgridPieces[subgridPieces.Count - 1]);
-                Debug.Log(testX + (testZ * mazeDimensions.x));
-                //mazeGrid[(int)Mathf.Clamp((x - 1) / mazeSubGridDimensions.x, 0, Mathf.Infinity), (int)Mathf.Clamp((z - 1) / mazeSubGridDimensions.y, 0, Mathf.Infinity)].Add(subgridPieces[subgridPieces.Count - 1]);
+                mazePieces.Add(new MazePiece(new Vector2(x, z)));
+                //Adds pieces to the correct grid
+                unvisitedGrids[
+                    (int)Mathf.Clamp((x - 1) / mazeSubGridDimensions.x, 0, Mathf.Infinity) +
+                    ((int)Mathf.Clamp((z - 1) / mazeSubGridDimensions.y, 0, Mathf.Infinity) *
+                    mazeDimensions.x)].Add(mazePieces[mazePieces.Count - 1]);
             }
         }
     }
@@ -79,26 +64,13 @@ public class MazeGeneratorSections : MonoBehaviour
 
     private void InstantiateMaze()
     {
-        foreach(var test in gridPieces)
+        foreach(var grid in closedGrids)
         {
-            foreach(var subPiece in test)
+            foreach(var piece in grid)
             {
-                if (subPiece.shouldPlace)
-                        Instantiate(mazePiecePrefab, new Vector3(subPiece.position.x * mazePieceSize.x, 0, subPiece.position.y * mazePieceSize.y), new Quaternion());
+                if (piece.shouldPlace)
+                        Instantiate(mazePiecePrefab, new Vector3(piece.position.x * mazePieceSize.x, 0, piece.position.y * mazePieceSize.y), new Quaternion());
             }
         }
-
-
-        //for (int z = 0; z < mazeGrid.GetLength(1); z++)
-        //{
-        //    for (int x = 0; x < mazeGrid.GetLength(0); x++)
-        //    {
-        //        for (int i = 0; i < mazeGrid[x, z].Count; i++)
-        //        {
-        //            if (mazeGrid[x, z][i].shouldPlace)
-        //                Instantiate(mazePiecePrefab, new Vector3(mazeGrid[x, z][i].position.x * mazePieceSize.x, 0, mazeGrid[x, z][i].position.y * mazePieceSize.y), new Quaternion());
-        //        }
-        //    }
-        //}
     }
 }
